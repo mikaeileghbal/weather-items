da8eaf09c952dc0bb776e6ece8d616a8f747b5c7
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2022-03-04 12:33:57 +0330

    Fix app to use class
    See: #10

--- code/app.js
+++ code/app.js
@@ -1,77 +1,27 @@
 const weatherApp = (function () {
-  const WEATHERR_ID = "296730df19d32c45959f5b8635c7e228";
-  const CITY_ID = "296730df19d32c45959f5b8635c7e228";
-  let urlAPI = `https://api.openweathermap.org/data/2.5/weather?q=tokyo&units=metric&appid=296730df19d32c45959f5b8635c7e228`;
-  let urlCity = `https://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=296730df19d32c45959f5b8635c7e228`;
-
-  let city = "";
-  let unit = "metric";
-  let lat = 0;
-  let lon = 0;
-
-  const app = document.getElementById("root");
   const currentTemp = document.getElementById("currentTemp");
   const currentDescription = document.getElementById("currentDescription");
   const currentCity = document.getElementById("currentCity");
   const getWeatherBtn = document.getElementById("getWeatherBtn");
   const cityInput = document.getElementById("city");
   const buttonUnit = document.querySelector(".button--unit");
+  let currentIcon = document.querySelector(".current-icon");
+  let unit = "metric";
+  const dailyrow = document.querySelector(".daily");
+  let today = new Date().getDay();
+  let city = "frankfurt";
 
   window.addEventListener("DOMContentLoaded", (event) => {
-    city = "frankfurt";
-    callWeather();
+    callWeather(unit);
   });
 
-  //get current weather
   getWeatherBtn.addEventListener("click", function (e) {
     e.preventDefault();
     city = cityInput.value;
     if (city !== "" && typeof city === "string") {
-      callWeather();
+      callWeather(unit);
     }
   });
-
-  function callWeather() {
-    setUrlCity(city);
-    getCityLocation(urlCity);
-  }
-
-  function setUrl(unit) {
-    urlAPI = new URL(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&&units=${unit}&appid=296730df19d32c45959f5b8635c7e228`
-      //`https://api.openweathermap.org/data/2.5/weather?${"units=" + unit + "&"}appid=296730df19d32c45959f5b8635c7e228`
-    );
-  }
-
-  function setUrlCity(city) {
-    urlCity = new URL(
-      `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=296730df19d32c45959f5b8635c7e228`
-    );
-    console.log("urlCity: ", urlCity);
-  }
-
-  function getWeatherData(url) {
-    fetch(url).then(getJson).then(displayResult).catch(errorHandler);
-    console.log("url: ", url);
-  }
-
-  function getCityLocation(url) {
-    fetch(url).then(getJson).then(displayCity).catch(errorHandler);
-  }
-
-  function displayCity(data) {
-    lat = data[0].lat;
-    lon = data[0].lon;
-
-    setUrl("metric");
-    getWeatherData(urlAPI);
-  }
-
-  async function displayWeather(url) {
-    let response = await fetch(url);
-    let data = await response.json();
-    return data;
-  }
 
   buttonUnit.addEventListener("click", (e) => {
     e.preventDefault();
@@ -85,26 +35,16 @@ const weatherApp = (function () {
     } else {
       setDegreeC(element);
     }
+    callWeather(unit);
   });
 
-  function setDegreeF(element) {
-    console.log("F");
-    element.parentNode.children[2].classList.add("left");
-    unit = "imperial";
-    setUrl("imperial");
-    getWeatherData(urlAPI);
+  function callWeather(un) {
+    const weather = new Weather("frankfurt");
+    weather.unit = un;
+    weather.getCurrentWeather(displayWeather);
   }
-
-  function setDegreeC(element) {
-    console.log("C");
-    element.parentNode.children[2].classList.remove("left");
-    unit = "metric";
-    setUrl("metric");
-    getWeatherData(urlAPI);
-  }
-
-  //callack function to represent actual data
-  function displayResult(jsonData) {
+  // const WEATHERR_ID = "296730df19d32c45959f5b8635c7e228";
+  function displayWeather(jsonData) {
     if (jsonData) {
       console.log("outer callback: ", jsonData);
       console.log(jsonData.current.temp);
@@ -114,25 +54,10 @@ const weatherApp = (function () {
         jsonData.current.temp
       )}&deg ${getUnitSymbol(unit)}</span>`;
       currentDescription.textContent = jsonData.current.weather[0].description;
-
       fillDailyWeatherItems(jsonData.daily);
     } else {
       displayErrorMessage("Empty data");
     }
-  }
-
-  let currentIcon = document.querySelector(".current-icon");
-
-  const dailyrow = document.querySelector(".daily");
-  let today = new Date().getDay();
-
-  function getDayName(dayNumber) {
-    const dayNames = ["Son", "Mon", "Tus", "Wed", "Thu", "Fri", "Sat"];
-    return dayNames[dayNumber];
-  }
-
-  function getUnitSymbol(unit) {
-    return unit === "metric" ? "C" : "F";
   }
 
   function fillDailyWeatherItems(daily) {
@@ -143,27 +68,23 @@ const weatherApp = (function () {
     for (let i = 0; i < daysCount; i++) {
       let { min, max } = daily[i].temp;
       let { main, icon } = daily[i].weather[0];
-
       let dayItem = `<div class="w-day">
-		<div class="w-day-name">
-			<span>${getDayName(today)}</span>
-		</div>
-		<div class="w-icon">
-			<img class="icon" src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="image" />
-			
-		</div>
-		<div class="w-temp">
-			<div class="temp-max">
-				<span>${Math.round(max)}&deg;</span>
-			</div>
-			<div class="temp-min">
-				<span>${Math.round(min)}&deg;</span>
-			</div>
-		</div>
-		</div>`;
-
+  	<div class="w-day-name">
+  		<span>${getDayName(today)}</span>
+  	</div>
+  	<div class="w-icon">
+  		<img class="icon" src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="image" />
+  	</div>
+  	<div class="w-temp">
+  		<div class="temp-max">
+  			<span>${Math.round(max)}&deg;</span>
+  		</div>
+  		<div class="temp-min">
+  			<span>${Math.round(min)}&deg;</span>
+  		</div>
+  	</div>
+  	</div>`;
       dailyrow.innerHTML += dayItem;
-
       today = today + 1;
       if (today > 6) {
         today = 0;
@@ -171,25 +92,110 @@ const weatherApp = (function () {
     }
   }
 
-  function getJson(response) {
-    //if not response ok return null
-    if (!response.ok) {
-      console.log("not OK");
-      return null;
-    }
-    //grab the type of response from response header
-    let type = response.headers.get("content-type");
-    //check to see if response type is as expected
-    //here we expect JSON data
-    if (type !== "application/json; charset=utf-8") {
-      throw new TypeError(`Expected JSON got ${type}`);
-    }
-    return response.json();
+  function getUnitSymbol(unit) {
+    return unit === "metric" ? "C" : "F";
   }
 
-  function errorHandler(e) {
-    console.log("Error: ", errMesage);
+  function getDayName(dayNumber) {
+    const dayNames = ["Son", "Mon", "Tus", "Wed", "Thu", "Fri", "Sat"];
+    return dayNames[dayNumber];
   }
+
+  function setDegreeF(element) {
+    console.log("F");
+    element.parentNode.children[2].classList.add("left");
+    unit = "imperial";
+  }
+  function setDegreeC(element) {
+    console.log("C");
+    element.parentNode.children[2].classList.remove("left");
+    unit = "metric";
+  }
+  // const CITY_ID = "296730df19d32c45959f5b8635c7e228";
+  // let urlAPI = `https://api.openweathermap.org/data/2.5/weather?q=tokyo&units=metric&appid=296730df19d32c45959f5b8635c7e228`;
+  // let urlCity = `https://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=296730df19d32c45959f5b8635c7e228`;
+  // let city = "";
+
+  // let lat = 0;
+  // let lon = 0;
+  // const app = document.getElementById("root");
+
+  // window.addEventListener("DOMContentLoaded", (event) => {
+  //   city = "frankfurt";
+  //   callWeather();
+  // });
+  // //get current weather
+  //
+  // function callWeather() {
+  //   setUrlCity(city);
+  //   getCityLocation(urlCity);
+  // }
+  // function setUrl(unit) {
+  //   urlAPI = new URL(
+  //     `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&&units=${unit}&appid=296730df19d32c45959f5b8635c7e228`
+  //     //`https://api.openweathermap.org/data/2.5/weather?${"units=" + unit + "&"}appid=296730df19d32c45959f5b8635c7e228`
+  //   );
+  // }
+  // function setUrlCity(city) {
+  //   urlCity = new URL(
+  //     `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=296730df19d32c45959f5b8635c7e228`
+  //   );
+  //   console.log("urlCity: ", urlCity);
+  // }
+  // function getWeatherData(url) {
+  //   fetch(url).then(getJson).then(displayResult).catch(errorHandler);
+  //   console.log("url: ", url);
+  // }
+  // function getCityLocation(url) {
+  //   fetch(url).then(getJson).then(displayCity).catch(errorHandler);
+  // }
+  // function displayCity(data) {
+  //   lat = data[0].lat;
+  //   lon = data[0].lon;
+  //   setUrl("metric");
+  //   getWeatherData(urlAPI);
+  // }
+  // async function displayWeather(url) {
+  //   let response = await fetch(url);
+  //   let data = await response.json();
+  //   return data;
+  // }
+
+  // //callack function to represent actual data
+  // function displayResult(jsonData) {
+  //   if (jsonData) {
+  //     console.log("outer callback: ", jsonData);
+  //     console.log(jsonData.current.temp);
+  //     currentCity.textContent = city; //jsonData.name;
+  //     currentIcon.src = ` http://openweathermap.org/img/wn/${jsonData.current.weather[0].icon}@2x.png`;
+  //     currentTemp.innerHTML = `<span>${Math.round(
+  //       jsonData.current.temp
+  //     )}&deg ${getUnitSymbol(unit)}</span>`;
+  //     currentDescription.textContent = jsonData.current.weather[0].description;
+  //     fillDailyWeatherItems(jsonData.daily);
+  //   } else {
+  //     displayErrorMessage("Empty data");
+  //   }
+  // }
+
+  // function getJson(response) {
+  //   //if not response ok return null
+  //   if (!response.ok) {
+  //     console.log("not OK");
+  //     return null;
+  //   }
+  //   //grab the type of response from response header
+  //   let type = response.headers.get("content-type");
+  //   //check to see if response type is as expected
+  //   //here we expect JSON data
+  //   if (type !== "application/json; charset=utf-8") {
+  //     throw new TypeError(`Expected JSON got ${type}`);
+  //   }
+  //   return response.json();
+  // }
+  // function errorHandler(e) {
+  //   console.log("Error: ", errMesage);
+  // }
 })();
 
 // // test classes
@@ -204,9 +210,3 @@ const weatherApp = (function () {
 // function displayCityData(data) {
 //   console.log("fetcher", data[0].lon);
 // }
-
-const weather = new Weather("miandoab");
-weather.getCurrentWeather(displayW);
-function displayW(data) {
-  console.log("Weather claas:", data);
-}
